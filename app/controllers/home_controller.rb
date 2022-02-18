e0b7eaf6e6190987c93bd0021868eb844b343116
“Takayuki <t-watanabe@watanabetakayukinoMacBook-Pro.local>
2022-02-18 17:13:33 +0900

    homeコントローラ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def index
+    p session[:cart]
+    cart = { 1 => { quantity: 1 } , 2 => { quantity: 4 } }
+    session[:cart] = cart
+
+    render plain: 'hello'
+  end
 end
