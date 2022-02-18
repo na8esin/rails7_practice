b4b0b4cfaa2323a561afb8c60f131cc927543c78
“Takayuki <t-watanabe@watanabetakayukinoMacBook-Pro.local>
2022-02-18 17:27:08 +0900

    コメント追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    # {1=>{:quantity=>1}, 2=>{:quantity=>4}}
+    # cookieとは明らかに違う
     p session[:cart]
     cart = { 1 => { quantity: 1 } , 2 => { quantity: 4 } }
     session[:cart] = cart
