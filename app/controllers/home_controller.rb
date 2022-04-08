86629cdd64d2b7a87db93d434ecbf2eb3c86d933
Takayuki Watanabe <watanabe@sikmi.com>
2022-04-08 12:10:32 +0900

    ajaxでhiddenタグを取得してdivタグの配下に追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_forgery_protection
+
   def index
     # {1=>{:quantity=>1}, 2=>{:quantity=>4}}
     # cookieとは明らかに違う
@@ -16,5 +18,15 @@ class HomeController < ApplicationController
 
   def sjis_post
     p params
+  end
+
+  def sjis_api_view; end
+
+  def sjis_api
+    render json: { result: '田中　検事'.encode(Encoding::SJIS) }, content_type: 'application/json; charset=sjis'
+  end
+
+  def sjis_api_html
+    render layout: false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get '/home/form_options_helper_select', to: 'home#form_options_helper_select'
   get '/home/sjis', to: 'home#sjis'
   post '/home/sjis_post', to: 'home#sjis_post'
+  get '/home/sjis_api_view', to: 'home#sjis_api_view'
+  post '/home/sjis_api', to: 'home#sjis_api'
+  post '/home/sjis_api_html', to: 'home#sjis_api_html'
 end
